f398750dcc591652147ac207d877e98cbc6b1fae
leo <80543539+inex1stent@users.noreply.github.com>
2021-12-13 09:47:16 -0300

    fix

---
+++ src/kiddings/jokes.ts
@@ -0,0 +1,12 @@
+export default {
+  jokes: {
+    // portuguese
+    "pt": ["Você conhece a piada do pônei? Pô nei eu...",
+    "Qual o animal que se dissolve na água? O orangoTANG.",
+    "Não deixe a obesidade se tornar um peso na sua vida!",
+    "O que a pata falou pro pato? Vem quá.",
+    "Qual é o rei dos queijos? O Reiqueijão."],
+    // english
+    "en": ["C", "D"]
+  }
+}
